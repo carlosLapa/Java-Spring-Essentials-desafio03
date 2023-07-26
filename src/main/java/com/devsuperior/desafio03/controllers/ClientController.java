66037c808a9651e06ac1c10768779a36e9c6c252
carlosLapa <carlosferreiralapa@gmail.com>
2023-07-26 17:40:33 +0100

    Client Repository, Client Controller, first postman test

--- src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
@@ -1,4 +1,25 @@
 package com.devsuperior.desafio03.controllers;
 
+import com.devsuperior.desafio03.entities.Client;
+import com.devsuperior.desafio03.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @GetMapping
+    public String teste () {
+        Optional<Client> result = clientRepository.findById(1L);
+        Client client = result.get();
+        return client.getName();
+    }
 }
